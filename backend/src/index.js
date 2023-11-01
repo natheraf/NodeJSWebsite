723061b8fc0688baa0ffe5d61b4975fe3a39b951
natheraf <ma.eric11214@outlook.com>
2023-11-01 19:21:33 -0400

    instead of copying, we point node server to frontend/build

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
   
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
